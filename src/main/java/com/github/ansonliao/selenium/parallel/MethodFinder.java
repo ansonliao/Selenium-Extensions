99eee8d325192ce16ba1144705788289dd4db8ec
Anson Liao <ansonliao.xiao@gmail.com>
2017-09-22 22:55:18 +0800

    Added find TestND methods in specified class by test group name (MethodFind.java).

--- src/main/java/com/github/ansonliao/selenium/parallel/MethodFinder.java
+++ src/main/java/com/github/ansonliao/selenium/parallel/MethodFinder.java
@@ -12,7 +12,8 @@ import java.util.stream.Collectors;
 public class MethodFinder {
     private static Logger logger = Logger.getLogger(MethodFinder.class);
 
-    public static List<Method> findAllAnnotatedTestMethodInClass(Class clazz) {
+    public static List<Method> findAllAnnotatedTestMethodInClass(
+            Class clazz) {
         logger.info("Find all @Test method in class: " + clazz.getName());
         return findMethodInClass(clazz).stream().distinct()
                 .filter(method -> method.isAnnotationPresent(Test.class))
@@ -25,29 +26,31 @@ public class MethodFinder {
                 .distinct().collect(Collectors.toList());
     }
 
-    public static List<Method> findTestNGMethodInClassByGroup(Class clazz, String groupName) {
+    public static List<Method> findTestNGMethodInClassByGroup(
+            Class clazz, String groupName) {
         List<Method> methodList = Lists.newArrayList();
 
         if (clazz.isAnnotationPresent(Test.class)) {
             Test t = (Test) clazz.getAnnotation(Test.class);
+            methodList.addAll(Lists.newArrayList(clazz.getDeclaredMethods()));
+
             if (t.groups().length > 0) {
                 List<String> groups = Lists.newArrayList(t.groups());
+                System.out.println("Class group: " + groups);
                 if (groups.contains(groupName)) {
-                    Lists.newArrayList(clazz.getDeclaredMethods())
-                            .stream().filter(method -> method.isAnnotationPresent(Test.class))
-                            .forEach(method -> methodList.add(method));
-                    return methodList;
+                    System.out.println("all method of class included");
+                    return Lists.newArrayList(clazz.getDeclaredMethods());
                 }
             }
-        } else {
-            return Lists.newArrayList(clazz.getDeclaredMethods())
-                    .stream()
-                    .filter(m -> m.isAnnotationPresent(Test.class))
-                    .filter(m ->
-                            Lists.newArrayList(m.getAnnotation(Test.class).groups()).contains(groupName))
-                    .collect(Collectors.toList());
         }
 
-        return methodList;
+        return Lists.newArrayList(clazz.getDeclaredMethods())
+                .stream()
+                .filter(m -> m.isAnnotationPresent(Test.class))
+                .filter(m ->
+                        Lists.newArrayList(
+                                m.getAnnotation(Test.class).groups())
+                                .contains(groupName))
+                .collect(Collectors.toList());
     }
 }
