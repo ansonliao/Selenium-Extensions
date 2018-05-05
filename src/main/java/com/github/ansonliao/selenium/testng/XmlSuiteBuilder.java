a96d727f22cf10ffaf50b15332067b23f92b0498
Anson Liao <ansonliao.xiao@gmail.com>
2018-05-06 05:17:37 +0800

    1. Refactor XmlSuiteBuilder.java 2. Try to make travis happy.
    issue: #33

--- src/main/java/com/github/ansonliao/selenium/testng/XmlSuiteBuilder.java
+++ src/main/java/com/github/ansonliao/selenium/testng/XmlSuiteBuilder.java
@@ -2,13 +2,13 @@ package com.github.ansonliao.selenium.testng;
 
 import com.github.ansonliao.selenium.utils.TestNGListenerUtils;
 import com.google.common.collect.ImmutableMultiset;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.xml.XmlSuite;
 
-import java.util.List;
-
 public class XmlSuiteBuilder {
+
     private static final Logger logger = LoggerFactory.getLogger(XmlSuiteBuilder.class);
     private static final List<String> defaultListeners = ImmutableMultiset.of(
             "com.github.ansonliao.selenium.testng.TestResultListener",
@@ -16,29 +16,50 @@ public class XmlSuiteBuilder {
 
     public static XmlSuite build() {
         XmlSuite xmlSuite = new XmlSuite();
-        xmlSuite.setName("Selenium Web UI Test");
-        xmlSuite.setParallel(XmlSuite.ParallelMode.TESTS);
-        xmlSuite.setPreserveOrder(false);
-        xmlSuite.setThreadCount(Runtime.getRuntime().availableProcessors());
-        xmlSuite.setVerbose(2);
+
+        // set generate xml suite information
+        xmlSuite = setXmlSuiteGenerateInfo(xmlSuite);
 
         // listeners
-        List<String> listeners = TestNGListenerUtils.getTestNGListeners();
-        if (listeners.size() != 0) {
-            logger.info("TestNG Listeners found: {}", listeners);
-            listeners.forEach(xmlSuite::addListener);
-        } else {
-            List<String> defaultListeners = TestNGListenerUtils.getDefaultTestNGListners();
-            logger.info("No TestNG Listener found, add default listeners: {}", defaultListeners);
-            defaultListeners.forEach(xmlSuite::addListener);
-        }
+        xmlSuite = addXmlSuiteListeners(xmlSuite);
 
-        XmlTestBuilder.setXmlSuite(xmlSuite);
-        XmlTestBuilder.build();
-        xmlSuite = XmlTestBuilder.getXmlSuite();
+        // add xml tests
+        // XmlTestBuilder.setXmlSuite(xmlSuite);
+        // XmlTestBuilder.build();
+        // xmlSuite = XmlTestBuilder.getXmlSuite();
+        xmlSuite = addXmlSuiteTests(xmlSuite);
         logger.info("\n" + xmlSuite.toXml());
 
         return xmlSuite;
+    }
+
+    public static XmlSuite setXmlSuiteGenerateInfo(XmlSuite suite) {
+        suite.setName("Selenium Web UI Test");
+        suite.setParallel(XmlSuite.ParallelMode.TESTS);
+        suite.setPreserveOrder(false);
+        suite.setThreadCount(Runtime.getRuntime().availableProcessors());
+        suite.setVerbose(2);
+        return suite;
+    }
+
+    public static XmlSuite addXmlSuiteListeners(XmlSuite suite) {
+        List<String> listeners = TestNGListenerUtils.getTestNGListeners();
+        if (listeners.size() != 0) {
+            logger.info("TestNG Listeners found: {}", listeners);
+            listeners.forEach(suite::addListener);
+            return suite;
+        }
+
+        List<String> defaultListeners = TestNGListenerUtils.getDefaultTestNGListners();
+        logger.info("No TestNG Listener found, add default listeners: {}", defaultListeners);
+        defaultListeners.forEach(suite::addListener);
+        return suite;
+    }
+
+    public static XmlSuite addXmlSuiteTests(XmlSuite suite) {
+        XmlTestBuilder.setXmlSuite(suite);
+        XmlTestBuilder.build();
+        return XmlTestBuilder.getXmlSuite();
     }
 
 }
